80edf06ff6ba77f8b7e37b9d2e4753be496218a4
davidoberst <juandavidbustosperez82@gmail.com>
2026-06-02 14:22:21 -0500

    Add client registration and menu options
    Implement main menu and client registration logic.

---
+++ C++/MATERIA - Algoritmos y Fundamentos de Programación/parcial2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main() {
+
+    int numero_de_cliente;
+    string nombre;
+    string num_direccion, calle_direccion, comuna_direccion, ciudad;
+    
+    double saldo = 0;
+    double descuento = 10; 
+    double limite_credito;
+
+   
+    int caso;
+    cout << "=========================================" << endl;
+    cout << "             BIENVENIDO    " << endl;
+    cout << "=========================================" << endl;
+
+    cout << "1. Registrar un cliente" << endl;
+    cout << "2. Mostrar el catalogo de articulos" << endl;
+    cout << "3. Realizar una compra" << endl;
+    cout << "4. Consultar fabrica" << endl; 
+    cout << "0. Salir" << endl;
+    
+    cout << "- Escriba una opcion: ";
+    cin >> caso; 
+
+    switch(caso) {
+        case 1:
+            //nombre 
+            cout << "\n--- REGISTRO DE CLIENTE ---" << endl;
+            cout <<"Inserte su numero de cliente : ";
+            cin >> numero_de_cliente;
+            cout<<" Inserte su nombre";
+            getline(cin,nombre);
+            
+            //direccion
+            cout<<"--Inserte su direccion-- :";
+            cout << "Inserte la Calle: ";
+            getline(cin >> ws, calle_direccion);
+            cout << "Inserte el Numero (casa/apto): ";
+            getline(cin >> ws, num_direccion);
+            cout << "Inserte la Comuna: ";
+            getline(cin >> ws, comuna_direccion);
+            cout << "Inserte la Ciudad: ";
+            getline(cin >> ws, ciudad);
+            
+            //datos finacnieros
+            cout<<"Inserte su limite de credito";
+            cin>>limite_credito;
+            if(limite_credito > 3000000){
+                cout<<"Limite de 3.000.000 excedido! el valor que ingreso fue ajustado a 3.000.000";
+                limite_credito = 3000000;
+            }
+            
+
+            break; 
+
+        case 2:
+           
+            break;
+
+        case 3:
+           
+            break;
+
+        case 0:
+            cout << "Saliendo del programa..." << endl;
+            break;
+
+        default:
+            cout << "Opcion no valida." << endl;
+    }
+
+    return 0;
+}
