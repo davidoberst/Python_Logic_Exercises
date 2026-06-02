5d868b2375ab4576a28cb15b2f2e9aea7cb4bbbf
davidoberst <juandavidbustosperez82@gmail.com>
2026-06-02 14:49:21 -0500

    Add client and product information in parcial2.cpp

--- C++/MATERIA - Algoritmos y Fundamentos de Programación/parcial2.cpp
+++ C++/MATERIA - Algoritmos y Fundamentos de Programación/parcial2.cpp
@@ -4,16 +4,44 @@
 using namespace std;
 
 int main() {
-
+    //cliente info
     int numero_de_cliente;
     string nombre;
     string num_direccion, calle_direccion, comuna_direccion, ciudad;
     
+    //cliente info monetaria
     double saldo = 0;
     double descuento = 10; 
     double limite_credito;
+    
 
-   
+    // --- VARIABLES ARTICULOS ---
+    // articulo 1
+    string art1 = "Teclado_Mecanico"; //art = articulo
+    double p1 = 150000;  //p = precio
+    int s1 = 10; //s = stock
+
+    // articulo 2
+    string art2 = "Mouse";
+    double p2 = 85000;
+    int s2 = 15;
+
+    // articulo 3
+    string art3 = "Monitor";
+    double p3 = 600000;
+    int s3 = 5;
+
+    // articulo 4
+    string art4 = "Audifonos";
+    double p4 = 80000;
+    int s4 = 8;
+
+    // articuloo 5
+    string art5 = "Luces";
+    double p5 = 40000;
+    int s5 = 12;
+    
+    //menu
     int caso;
     cout << "=========================================" << endl;
     cout << "             BIENVENIDO    " << endl;
@@ -79,7 +107,33 @@ int main() {
             break; 
 
         case 2:
-           
+           cout << "\n=========================================" << endl;
+            cout << "         CATALOGO DE PRODUCTOS           " << endl;
+            cout << "=========================================" << endl;
+            cout << fixed << setprecision(0); 
+            
+            cout << "1- " << art1 << endl;
+            cout << "       Precio: $" << p1 << " | Stock: " << s1 << " unid." << endl;
+            cout << "-----------------------------------------" << endl;
+            
+            cout << "2- " << art2 << endl;
+            cout << "       Precio: $" << p2 << " | Stock: " << s2 << " unid." << endl;
+            cout << "-----------------------------------------" << endl;
+            
+            cout << "3- " << art3 << endl;
+            cout << "       Precio: $" << p3 << " | Stock: " << s3 << " unid." << endl;
+            cout << "-----------------------------------------" << endl;
+            
+            cout << "4- " << art4 << endl;
+            cout << "       Precio: $" << p4 << " | Stock: " << s4 << " unid." << endl;
+            cout << "-----------------------------------------" << endl;
+            
+            cout << "5- " << art5 << endl;
+            cout << "       Precio: $" << p5 << " | Stock: " << s5 << " unid." << endl;
+            
+            cout << "=========================================" << endl;
+            cout << "Fabrica proveedora:TiendaTecnologica.com" << endl;
+            cout << "=========================================" << endl;
             break;
 
         case 3:
